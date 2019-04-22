d1629ad190b369d9d5d155801a7c3e8f7c482f2e
Vincent Nguyen <vincentnguyenthanhtrung@gmail.com>
2019-04-22 09:54:38 -0500

    add list redirections and manipulate YAML

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ For instance, if we have a redirect setup for `/dogs` to `https://www.somesite.c
 ## Requirements
 
 The list of redirection should be maintained in a command line tool, what can:
-- [ ] Manipulate YAML config file. Where the redirection list peristently stored.
+- [x] Manipulate YAML config file. Where the redirection list peristently stored.
 - [ ] Implement append to the list: `urlshorten configure -a dogs -u www.dogs.com` 
 - [ ] Implement remove from the list: `urlshorten -d dogs`
-- [ ] List redirections: `urlshorten -l`
+- [x] List redirections: `urlshorten -l`
 - [ ] Run HTTP server on a given port: `urlshorten run -p 8080`
 - [x] Prints usage info: `urlshorten -h`
 
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
 )
 
 //URL stores both original and shorten url
@@ -11,23 +15,69 @@ type URL struct {
 	shortenURL  string
 }
 
+type flagStorage struct {
+	addURL    *bool
+	stringURL *string
+	removeURL *string
+	listURL   *bool
+	port      *int64
+	help      *bool
+	run       *bool
+	configure *bool
+}
+
 var serialData map[string]string
 
 func main() {
 
-	add := flag.Bool("a", false, "Implement append to the list: `urlshorten configure -a dogs -u www.dogs.com`")
-	url := flag.String("u", "", "Implement append to the list: `urlshorten configure -a dogs -u www.dogs.com`")
-	rem := flag.String("d", "", "Implement remove from the list: `urlshorten -d dogs`")
-	lis := flag.Bool("l", false, "List redirections: `urlshorten -l`")
-	port := flag.Int64("p", 8080, "Run HTTP server on a given port: `urlshorten run -p 8080`")
-	help := flag.Bool("h", false, "Prints usage info: `urlshorten -h`")
+	fs := flagStorage{}
+	initFlag(&fs)
+	readYAML()
 
-	flag.Parse()
-
-	//help flag
-	if *help == true {
+	if *fs.help {
 		flag.PrintDefaults()
 	}
+	if *fs.listURL {
+		printYAML()
+	}
 
-	fmt.Println(*add, *url, *rem, *lis, *port)
+	// if *port == 0 {
+	// 	http.HandleFunc("/", handler)
+	// }
+
+}
+
+func initServer() {
+
+}
+
+func initFlag(fs *flagStorage) {
+	fs.addURL = flag.Bool("a", false, "Implement append to the list: `urlshorten configure -a dogs -u www.dogs.com`")
+	fs.stringURL = flag.String("u", "", "Implement append to the list: `urlshorten configure -a dogs -u www.dogs.com`")
+	fs.removeURL = flag.String("d", "", "Implement remove from the list: `urlshorten -d dogs`")
+	fs.listURL = flag.Bool("l", false, "List redirections: `urlshorten -l`")
+	fs.port = flag.Int64("p", 8080, "Run HTTP server on a given port: `urlshorten run -p 8080`")
+	fs.help = flag.Bool("h", false, "Prints usage info: `urlshorten -h`")
+	fs.configure = flag.Bool("c", false, "Configure the app")
+	fs.run = flag.Bool("r", false, "Run the app")
+
+	flag.Parse()
+}
+
+func readYAML() {
+	bytes, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(bytes, &serialData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printYAML() {
+	for k, v := range serialData {
+		fmt.Printf("%v : %v\n", k, v)
+	}
 }
